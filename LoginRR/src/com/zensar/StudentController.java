fade97c2bcc709c4f492e93e94d23fc3a6ec4d14
ShounaKulkarni <shounakkulkarni9@gmail.com>
2021-01-11 10:06:02 +0530

    Second commit!

--- LoginRR/src/com/zensar/StudentController.java
+++ LoginRR/src/com/zensar/StudentController.java
@@ -17,77 +17,9 @@ public class StudentController extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) {
 
 		String requestAction = request.getParameter("requestAction");
-		if(requestAction.equals("getAllS")) {
-			StudentRepository sr = new StudentRepository();
-			List<BeanStudent> listOfAllStudent = sr.getAllStudent();
-			System.out.println(listOfAllStudent);
-
-			RequestDispatcher rd = request.getRequestDispatcher("showAllStudent.jsp");
-			request.setAttribute("listOfAllStudent",listOfAllStudent );
-			try {
-				rd.forward(request, response);
-
-			} catch (Exception e) {
-				System.out.println("doGet Catch"+e);
-			}
-		}//if
-		else if(requestAction.equals("delete")) {
-			int sid = Integer.parseInt(request.getParameter("sid"));
-			StudentRepository sr = new StudentRepository();
-			sr.deleteStudent(sid);
-			try {
-				//rd.forward(request, response);
-				response.sendRedirect("index.jsp");
-			} catch (Exception e) {
-				System.out.println("doGet deleteStudent catch"+e);
-			}
-
-		}//else if
-		else if(requestAction.equals("updateS")) {
-			StudentRepository rs = new StudentRepository();
-			int sid = Integer.parseInt(request.getParameter("sid"));
-			BeanStudent bs = rs.getStudent(sid);
-			RequestDispatcher rd = request.getRequestDispatcher("updateStudentForm.jsp");
-			request.setAttribute("bs",bs);
-			try {
-				rd.forward(request, response);
-			} catch (Exception e) {
-
-			}
-		}
-		else if(requestAction.equals("updateDB"))	{
-			int sid = Integer.parseInt(request.getParameter("sid"));
-			String sname = request.getParameter("sname");
-			String scourse = request.getParameter("scourse");
-			String shometown = request.getParameter("shometown");
-			String sgender = request.getParameter("sgender");
-			BeanStudent bs = new BeanStudent(sid, sname, scourse, shometown, sgender);
-			StudentRepository rs = new StudentRepository();
-			rs.updateStudent(bs);
-			try {
-				response.sendRedirect("index.jsp");
-			} catch (IOException e) {
-
-				e.printStackTrace();
-			}
-
-		}
-		else if(requestAction.equals("addDB")) {
-			int sid = Integer.parseInt(request.getParameter("sid"));
-			String sname = request.getParameter("sname");
-			String scourse = request.getParameter("scourse");
-			String shometown = request.getParameter("shometown");
-			String sgender = request.getParameter("sgender");
-			BeanStudent bs = new BeanStudent(sid, sname, scourse, shometown, sgender);
-			StudentRepository sr = new StudentRepository();
-			sr.addStudent(bs);
-			try {
-				response.sendRedirect("index.jsp");
-			} catch (Exception e) {
-				System.out.println("doPost catch"+e);
-			}
-		}
-		else if(requestAction.equals("loginFormjsp")) {
+		 
+		 
+		if(requestAction.equals("loginFormjsp")) {
 			String susername = request.getParameter("susername");
 			String spassword = request.getParameter("spassword");
 
