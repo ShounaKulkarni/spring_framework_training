441fb63ca3646a73447e302021ace4b3ac8c3404
ShounaKulkarni <shounakkulkarni9@gmail.com>
2021-01-11 10:12:01 +0530

    third commit!

--- LoginRR/src/com/zensar/StudentRepository.java
+++ LoginRR/src/com/zensar/StudentRepository.java
@@ -10,9 +10,9 @@ public class StudentRepository {
 	public boolean loginCheck(String susername, String spassword) {
 		boolean result = false;
 		Connection con = DBUtil.getMysqlDBConnection();
-		String addStudentSql = "SELECT * FROM login_form WHERE username=?";
+		String check = "SELECT * FROM login_form WHERE username=?";
 		try {
-			PreparedStatement pst = con.prepareStatement(addStudentSql);
+			PreparedStatement pst = con.prepareStatement(check);
 			pst.setString(1, susername); 
 			ResultSet rs = pst.executeQuery();
 			while(rs.next()) {
